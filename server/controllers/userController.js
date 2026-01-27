65bf3af383ffdb16af4e118cef07540549951307
Mattias <mattias.bygge@gmail.com>
2026-01-27 15:33:30 +0100

    finished BE endpoint to create new user

---
+++ server/controllers/userController.js
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+import { User } from '../db.js';
+
+
+export const registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // check that both fields are filled in:
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Username and password required' });
+    }
+
+    // check if the username is already used:
+    const existingUser = await User.findOne({ where: { username: username } });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Username already exists' });
+    }
+
+    // hash password:
+    const hash = await bcrypt.hash(password, 10);
+
+    // create user:
+    const newUser = await User.create({
+      username: username,
+      password: hash,
+    })
+
+    //remove the hashed data from return value:
+    const returnedUserData = newUser.toJSON();
+    delete returnedUserData.password;
+
+    //return res:
+    return res.status(201).json(returnedUserData);
+
+    // catch error:
+  } catch (err) {
+    console.error('registerUser error:', err);
+    return res.status(500).json({ message: 'Failed to create new user' });
+  }
+};
