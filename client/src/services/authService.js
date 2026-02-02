6006cf036c30d5a5082462275c4cc13fb5b5650a
Michi <jagar008@gmail.com>
2026-02-02 12:39:23 +0100

    fix: remove a duplicated code

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -16,16 +16,11 @@ export const register = async (username, password) => {
   }
 };
 
-const API = axios.create({
-    baseURL: API_URL, 
-    withCredentials: true // Required for session cookies
-});
-
 export const loginUser = async (username, password) => {
-    try {
-        const response = await API.post('/login', { username, password });
-        return response.data;
-    } catch (error) {
-        throw error.response?.data?.message || "Server Error";
-    }
+  try {
+    const response = await API.post("/login", { username, password });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.message || "Server Error";
+  }
 };
